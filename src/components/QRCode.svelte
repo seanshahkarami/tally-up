b6475d27a5ffce50b5466724da72a0f048b4021c
Sean Shahkarami <sean.shahkarami@gmail.com>
2025-05-31 22:59:18 -0500

    updated state management to support url and storage. added qr code for handoff.

---
+++ src/components/QRCode.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { onMount } from "svelte";
+    let { text } = $props();
+    let qrContainer;
+
+    onMount(() => {
+        if (!window.QRCode) {
+            console.error(
+                "QRCode library not found. Make sure it's loaded in index.html.",
+            );
+            return;
+        }
+
+        new QRCode(qrContainer, {
+            text,
+            width: 256,
+            height: 256,
+        });
+    });
+</script>
+
+<div bind:this={qrContainer}></div>
+<p class="text-center text-xs text-gray-500 py-2">{text}</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,78 @@
 <script lang="ts">
     import "../app.css";
+    import QrCode from "../components/QRCode.svelte";
 
     const isBrowser = !(typeof window === "undefined");
 
-    let total = $state(0);
-    let current = $state(0);
+    type State = {
+        name: string;
+        total: number;
+        current: number;
+        version: number;
+    };
 
-    if (isBrowser) {
-        total = parseInt(localStorage.getItem("total") || "0");
-        current = parseInt(localStorage.getItem("current") || "0");
+    function getStateFromStorage(): State | null {
+        const storageState = localStorage.getItem("state");
+        if (storageState == null) {
+            return null;
+        }
+        const [total, current, version] = storageState.split(",").map(Number);
+        return { name: "storage", total, current, version };
     }
 
+    function getStateFromUrl(): State | null {
+        const urlParams = new URLSearchParams(window.location.search);
+        const paramState = urlParams.get("state");
+        if (paramState == null) {
+            return null;
+        }
+
+        const [total, current, version] = paramState.split(",").map(Number);
+        return { name: "url", total, current, version };
+    }
+
+    function getLatestState(): State {
+        let states: State[] = [
+            { name: "default", total: 0, current: 0, version: 0 },
+        ];
+
+        const storageState = getStateFromStorage();
+        if (storageState) {
+            states.push(storageState);
+        }
+
+        const urlState = getStateFromUrl();
+        if (urlState) {
+            states.push(urlState);
+        }
+
+        // Return the newest state.
+        return states.sort((a, b) => b.version - a.version)[0];
+    }
+
+    let total = $state(0);
+    let current = $state(0);
+    let version = $state(0);
+    let isSharing = $state(false);
+
+    let address = "";
+
+    if (isBrowser) {
+        address = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
+    }
+
+    let shareText = $derived(
+        `${address}/?state=${total},${current},${version}`,
+    );
     let audioCtx: AudioContext | null = null;
 
     if (isBrowser) {
+        const latestState = getLatestState();
+        console.log("Initial state is", latestState.name);
+        total = latestState.total;
+        current = latestState.current;
+        version = latestState.version;
+
         audioCtx = new window.AudioContext();
     }
 
@@ -39,12 +98,9 @@
     function inc() {
         total++;
         current++;
+        version++;
         if (isBrowser) {
-            localStorage.setItem("total", total.toString());
-            localStorage.setItem("current", current.toString());
-        }
-        if (navigator.vibrate) {
-            navigator.vibrate(30);
+            localStorage.setItem("state", `${total},${current},${version}`);
         }
         playBeep(30, 440);
     }
@@ -54,8 +110,9 @@
             return;
         }
         current--;
+        version++;
         if (isBrowser) {
-            localStorage.setItem("current", current.toString());
+            localStorage.setItem("state", `${total},${current},${version}`);
         }
         playBeep(30, 220);
     }
@@ -74,9 +131,9 @@
             clearResetTimer();
             total = 0;
             current = 0;
+            version = 0;
             if (isBrowser) {
-                localStorage.setItem("total", total.toString());
-                localStorage.setItem("current", current.toString());
+                localStorage.setItem("state", `${total},${current},${version}`);
             }
         }, 1000);
     }
@@ -90,35 +147,48 @@
     }
 </script>
 
-<div class="fixed inset-0 flex items-center justify-center">
-    <div class="text-4xl leading-normal">
-        {#if resetInterval != null}
-            <p class="text-lg">
-                Hold to reset tally in {resetCountdown} seconds...
-            </p>
-        {:else}
-            <p><strong>{total}</strong> <small>total</small></p>
-            <p><strong>{current}</strong> <small>current</small></p>
-        {/if}
+{#if isSharing}
+    <div class="fixed inset-0 flex items-center justify-center">
+        <div class="text-4xl leading-normal">
+            <QrCode text={shareText} />
+        </div>
     </div>
-</div>
+{:else}
+    <div class="fixed inset-0 flex items-center justify-center">
+        <div class="text-4xl leading-normal">
+            {#if resetInterval != null}
+                <p class="text-lg">
+                    Hold to reset tally in {resetCountdown} seconds...
+                </p>
+            {:else}
+                <p><strong>{total}</strong> <small>total</small></p>
+                <p><strong>{current}</strong> <small>current</small></p>
+            {/if}
+        </div>
+    </div>
+
+    <button
+        class="bg-red-400 w-24 h-24 rounded-full text-4xl fixed top-6 left-6 select-none"
+        onclick={dec}>-</button
+    >
+
+    <button
+        class="bg-green-400 w-24 h-24 rounded-full text-4xl fixed bottom-6 right-6 select-none"
+        onclick={inc}>+</button
+    >
+
+    <button
+        class="bg-gray-400 w-24 h-24 rounded-full text-4xl fixed top-6 right-6 select-none"
+        onmousedown={startResetTimer}
+        ontouchstart={startResetTimer}
+        onmouseup={clearResetTimer}
+        onmouseleave={clearResetTimer}
+        ontouchend={clearResetTimer}
+        ontouchcancel={clearResetTimer}>c</button
+    >
+{/if}
 
 <button
-    class="bg-red-400 w-24 h-24 rounded-full text-4xl fixed top-6 left-6 select-none"
-    onclick={dec}>-</button
->
-
-<button
-    class="bg-green-400 w-24 h-24 rounded-full text-4xl fixed bottom-6 right-6 select-none"
-    onclick={inc}>+</button
->
-
-<button
-    class="bg-gray-400 w-24 h-24 rounded-full text-4xl fixed top-6 right-6 select-none"
-    onmousedown={startResetTimer}
-    ontouchstart={startResetTimer}
-    onmouseup={clearResetTimer}
-    onmouseleave={clearResetTimer}
-    ontouchend={clearResetTimer}
-    ontouchcancel={clearResetTimer}>c</button
+    class="bg-blue-400 w-24 h-24 rounded-full text-4xl fixed bottom-6 left-6 select-none"
+    onclick={() => (isSharing = !isSharing)}>s</button
 >
